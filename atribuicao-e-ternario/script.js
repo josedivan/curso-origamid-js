789b52ec96d05659013760205e3a0bbeb7958720
Edivan <edivanlima23@gmail.com>
2021-02-17 23:17:30 -0300

    initial

---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,35 @@
+//var numero = 20;
+//var numero2 = 10;
+//
+//numero += numero2; // numero = numero + numero2
+//console.log(numero);
+
+// OPERADORES TERNARIO //
+
+//var idade = 18;
+//var naoPossuiDiabete = true;
+//var podeBeber;
+//
+//podeBeber = idade >= 18 && naoPossuiDiabete ? "Pode beber" : "Não pode beber";
+//
+//console.log(podeBeber);
+
+var possuiCarro = false;
+var possuiCasa = true;
+var darCredito;
+
+darCredito =
+  possuiCarro && possuiCasa ? "Possui crédito" : "Não possui crédito";
+console.log(darCredito);
+
+var javscript = false;
+var angular = true;
+
+var varga;
+
+varga =
+  javscript && angular
+    ? "Aprovado para a vaga"
+    : "Não foi selecionado para a vaga";
+
+console.log(varga);
---
+++ escopo_de_funcoes/script.js
@@ -0,0 +1,11 @@
+var semana = "sexta"; // Pode ser atribuida durante o uso da variável
+const semana = "sexta"; // Não pode ser atribuida durante o uso da variável
+semana = "quinta";
+
+console.log(semana);
+
+let ano;
+ano = 2018;
+ano++;
+console.log(ano); // 2019
+let ano = 2020; // erro, redeclarou a variável
