779e3b0d9b7f6922bc9cf9bc492289a1641e0d96
Edivan <edivanlima23@gmail.com>
2021-02-21 23:18:01 -0300

    classes-atributos

---
+++ animais-fantasticos/js/classes-e-atributos.js
@@ -0,0 +1,11 @@
+//pegando a classe menu com vetor 0
+const menu = document.querySelector(".menu");
+// adicionando uma classe ativo
+menu.classList.add("ativo", "teste");
+// removendo a classe ativo
+menu.classList.remove("ativo");
+menu.classList.toggle("ativo");
+
+//console.log(menu);
+const animamis = document.querySelector(".animais");
+console.log(animamis.attributes);
