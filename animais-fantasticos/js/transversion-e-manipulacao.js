d5b8fa10ea5684741f119efb18755db491150be8
Edivan <edivanlima23@gmail.com>
2021-03-05 07:39:07 -0300

    transversion-e-manipulacao

--- animais-fantasticos/js/transversion-e-manipulacao.js
+++ animais-fantasticos/js/transversion-e-manipulacao.js
@@ -1,7 +1,31 @@
+//const h1 = document.querySelector("h1");
+//
+//console.log(h1.innerHTML);
+//console.log(h1.outerHTML);
+//
+//const lista = document.querySelector(".animais-lista");
+//console.log(lista.children[--lista.children.length]);
+//
+//const animais = document.querySelector(".animais");
+//
+//const contato = document.querySelector(".contato");
+//const titulo = document.querySelector(".titulo");
+//
+//const mapa = document.querySelector(".mapa");
+//
+////contato.removeChild(titulo);
+//
+//contato.insertBefore(animais, mapa);
+//
+//const novoh1 = document.createElement("h1");
+//novoh1.innerText = "Novo titulo ";
+//novoh1.classList.add("titulo");
+//mapa.appendChild(novoh1);
+//console.log(novoh1);
+
 const h1 = document.querySelector("h1");
+const faq = document.querySelector(".faq");
 
-console.log(h1.innerHTML);
-console.log(h1.outerHTML);
-
-const lista = document.querySelector(".animais-lista");
-console.log(lista.children[--lista.children.length]);
+const cloneH1 = h1.cloneNode(true);
+cloneH1.classList.add("azul");
+faq.appendChild(cloneH1);
